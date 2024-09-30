49f8f48fcad090a770bdb3110e875dd0bb985664
kyle <kp3crt@bolton.ac.uk>
2024-09-30 10:35:57 +0100

    Updated with Count all lines in text file.py

---
+++ Session 2/Count all lines in text file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from future.backports import count
+
+try:
+    numoflines = numofchars = 0
+    file = open('grumpy.txt', 'r')
+# this will open the separate file in read mode so that the file can be read
+    lines = file.readlines()
+    for line in lines:
+        numoflines += 1
+        for char in line:
+            numofchars += 1
+    file.close()
+    print("Characters in file:", numofchars)
+    print("Lines in file:", numoflines)
+except FileNotFoundError as error:
+    print("File could not be found:", str(error))
