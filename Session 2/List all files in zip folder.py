5292599f84df1e1a8ad50807392bcb7dceb8894d
kyle <kp3crt@bolton.ac.uk>
2024-09-30 10:23:54 +0100

    First commit

---
+++ Session 2/List all files in zip folder.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import zipfile
+try:
+    def List_all_files_in_zip(filename):
+        with zipfile.ZipFile(filename) as myzip:
+            for zipinfo in myzip.infolist():
+                yield zipinfo.filename
+# the function List_all_files_in_zip will list all the files in the zip file
+    for filename in List_all_files_in_zip("files.zip"):
+        print(filename)
+# when the code is ran the output will be printed to the screen to show which files are in the zip file
+except:
+    print("An error occurred")
+# try and except will handle an error if one occurs and print out an error message
