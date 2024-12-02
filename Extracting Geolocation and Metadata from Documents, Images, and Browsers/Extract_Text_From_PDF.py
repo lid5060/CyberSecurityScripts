779e77c3b16b913070bab144d4719b03930f0f5d
lid5060 <kp3crt@bolton.ac.uk>
2024-12-02 11:28:43 +0000

    Extracting Geolocation and Metadata from Documents, Images, and Browsers

---
+++ Extracting Geolocation and Metadata from Documents, Images, and Browsers/Extract_Text_From_PDF.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+
+import PyPDF2
+
+pdf_file = open("PDF/10-page-sample.pdf","rb")
+pdfReader = PyPDF2.PdfFileReader(pdf_file)
+
+page_numbers = input("Enter page number:")
+page_object = pdfReader.getPage(int(page_numbers)-1)
+pdf_text = str(page_object.extractText())
+
+print(pdf_text)
